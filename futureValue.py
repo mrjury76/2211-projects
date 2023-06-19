28808581e24506cf08e753c40255bb23729708b4
mrjury76 <mrjury76@gmail.com>
2023-06-19 15:12:04 -0700

    yes

--- futureValue.py
+++ futureValue.py
@@ -3,15 +3,20 @@
 # Version: 1.0
 #   this is the future value program that takes some input such as inital value
 #   and time and interest per amount of time and displays the future value of the
-#   interest
+#   compounding interest
 
 
 def future_value():
-    present = float(input('Enter the present value of the account: '))
-    interest = float(input('Enter the monthly interest rate from 0%-100%: '))
+    present = float(input('\nEnter the present value of the account: '))
+    interest = float(input('Enter the monthly interest rate from 0-100%: '))
     months = float(input('Enter the number of months: '))
 
     future = present * (1 + interest / 100) ** months
+    change = future - present
+
+    print('\nWith an initial value of $', format(present, '0,.2f'), ', over a period of ', format(months, '0,.0f'), 
+        ' months, and an interest rate of ', interest, '% per month, the future value of your account will be: ', sep='')
     print('$', format(future, '0,.2f'), sep='')
+    print('Thats an increase of $', format(change, '0,.2f'), '!\n', sep = '')
     
 future_value()
