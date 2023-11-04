adf3ff5860a6ebf9a49b330a005022406813db84
mrjury76 <mrjury76@gmail.com>
2023-11-04 16:37:20 -0700

    Improved comments and added a failed attempts msg

--- password_generator.py
+++ password_generator.py
@@ -1,8 +1,9 @@
 # Author: Patrick Thrower
 # Student ID: T00620480
 # Date: Nov 4, 2023
-# Version: 2.0
-# Description: This is a password and username generator program.
+# Version: 2.1
+# Description: This is a username generator program that takes inputs from the user and turns it into a standard use username. It also takes an inputted password
+#  from the user and validates it if within set parameters.
 
 #main function that takes input from user and calls both functions
 def main():  
@@ -11,10 +12,10 @@ def main():
     student_id = input("Please enter your student ID: ")
     get_login_name(first_name, last_name, student_id)  # Calls get login function to make the login username
 
-    print("\nPlease create a password. A valid password must be at least seven characters in length, Must have at least one uppercase letter, one lowercase letter, and one digit:")
+    print("\nPlease create a password. A valid password must be at least seven characters in length, must have at least one uppercase letter, one lowercase letter, and one digit:")
     validate_password()
 
-# A function that takes the first 3 char of the users first and last name and the last 3 digits of their student id and concats into a username string
+# A function that takes the first 3 characters of the users first and last name and the last 3 digits of their student id and concatenates into a username string
 def get_login_name(first_name, last_name, student_id):  
     username = first_name[:3].capitalize() + last_name[:3].capitalize() + student_id[-3:]
     print(username)
@@ -24,7 +25,7 @@ def validate_password():
     x = 4
     while x > 0:
         flag = True
-        password = input()  # Assigns the users input into password and then passes that into the validate_password function
+        password = input() 
 
         if (password.islower()):
             flag = False
@@ -47,7 +48,10 @@ def validate_password():
             break
 
         elif(flag == False):
-            print("\nYou have", x-1, "attempts remaining\n")
-            x -= 1
+            if(x==0):
+                print("\nYou have used all your attempts\nPlease try again :)")
+            else:
+                print("\nYou have", x-1, "attempts remaining\n")
+                x -= 1
     
 main()
